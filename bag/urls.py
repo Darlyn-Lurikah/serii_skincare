a63b42ded1da17b70a0ab3d90480e7a7429612c3
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2021-11-15 18:07:59 +0000

    Create bag template dir and html

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,8 @@
+from django.urls import path
 from . import views
 
 urlpatterns = [
 
     # Empty path shows its root url
-    path('', views.view_bag, name='bag')
+    path('', views.view_bag, name='view_bag')
 ]
