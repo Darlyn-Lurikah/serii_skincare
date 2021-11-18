59f998c2f18781856f8630875802b045efb2e172
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2021-11-18 20:53:57 +0000

    Added add to bag view

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,28 @@ def view_bag(request):
     """ A view to show the shopping bag page """
 
     return render(request, 'bag/bag.html')
+
+
+# View to add products to bag
+def add_to_bag(request, item_id):
+
+    """We get from the POST form data the quantity & redirect (template 'name' attr) """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    """ To keep bag items until browser closed 
+    we find var 'bag_session' in session. If its not 
+    there, we initialise to empty dict ready for items """
+    bag_session = request.session.get('bag_session', {})
+
+    # if item_id matches one in bag_session list
+    # ie.in bag already increment quantity
+    if item_id in list(bag_session.keys()):
+        bag_session[item_id] += quantity
+
+    # Else add id to list ie. add to bag
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag_session'] = bag_session
+    return redirect(redirect_url)
