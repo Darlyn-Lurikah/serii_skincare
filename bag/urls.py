6a2709561e703a86e7de6439e05f11b39646ea50
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2021-11-22 17:05:36 +0000

    Added update quantity from bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # Empty path shows its root url
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -37,24 +37,19 @@ def add_to_bag(request, item_id):
 # View to adjust bag quantity 
 def adjust_bag(request, item_id):
 
-    """We get from the POST form data the quantity & 
-    redirect (template 'name' attr) """
+    """ We get from the POST form data the quantity """
     quantity = int(request.POST.get('quantity'))
-
 
     """ To keep bag items until browser closed 
     we find var 'bag_session' in session. If its not 
     there, we initialise to empty dict ready for items """
     bag_session = request.session.get('bag_session', {})
-
-    # if item_id matches one in bag_session list
-    # ie.in bag already increment quantity
+    
     if quantity > 0:
         bag_session[item_id] = quantity
 
-    # Else add id to dict ie. add to bag
     else:
         bag_session.pop[item_id]
 
     request.session['bag_session'] = bag_session
-    return redirect(reverse(view_bag))
+    return redirect(reverse('view_bag'))
