ea55eb443c20b71946c95b48e24066ce2f173925
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2021-11-25 19:17:59 +0000

    Added view for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    # Get the bag from the session
+    bag_session = request.session.get('bag_session', {})
+    if not bag_session:
+        # Error message & redirect if nothing in bag 
+        messages.error(request, "Your bag is empty")
+        return redirect(reverse('products'))
+
+    # Create instance of OrderForm
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    # Save instance var in context
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
