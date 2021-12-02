67a073a5000726801c49227a05156c68744a5d97
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2021-12-02 12:14:05 +0000

    Added Stripe js and css styling

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,7 +5,30 @@ var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 // Create instance of stripe
 var elements = stripe.elements();
+
+const appearance = {
+    theme: 'bubblegum',
+    variables: {
+      fontWeightNormal: '500',
+      borderRadius: '5px',
+      colorBackground: 'white',
+      colorPrimary: '#DF1B41',
+      colorPrimaryText: 'white',
+      spacingGridRow: '15px'
+    },
+    rules: {
+      '.Label': {
+        marginBottom: '6px'
+      },
+      '.Tab, .Input, .Block': {
+        boxShadow: '0px 3px 10px rgba(18, 42, 66, 0.08)',
+        padding: '12px'
+      }
+    }
+  };
+  
+
 // Use instance to make card element 
-var card = elements.create('card');
+var card = elements.create('card', {appearance: appearance});
 // Mount card element to attr w/ this id 
 card.mount('#card-element');
