8573c81c4c26a5e283455d619d6dbf6e8ded3453
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2021-12-03 22:48:54 +0000

    Imported contexts from bag app to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
 
 def checkout(request):
@@ -12,6 +13,13 @@ def checkout(request):
         messages.error(request, "Your bag is empty")
         return redirect(reverse('products'))
 
+    # Save our imported context to a var
+    current_bag = bag_contents(request)
+    # From context take grand total 
+    total = current_bag('grand_total')
+    # Round to 0 decimals as Stripe needs integer
+    stripe_total = round(total * 100)
+    
     # Create instance of OrderForm
     order_form = OrderForm()
     template = 'checkout/checkout.html'
